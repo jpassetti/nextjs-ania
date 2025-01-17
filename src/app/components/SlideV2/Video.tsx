dcb1ce33f840cdfe6b820cc3cded3a8fbaba8f37
Jeff Passetti <jpassetti@gmail.com>
2025-01-16 19:47:29 -0500

    video autoplay

--- src/app/components/SlideV2/Video.tsx
+++ src/app/components/SlideV2/Video.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import classNames from 'classnames/bind';
 import styles from './subcomponents.module.scss';
 
@@ -11,26 +11,70 @@ interface VideoProps {
   metaInformation?: string;
 }
 
-// videoUrl is the Vimeo URL of the video
 const Video: React.FC<VideoProps> = ({ videoUrl, coverImage, caption, metaInformation }) => {
- //console.log({ videoUrl});
+  const [autoplayed, setAutoplayed] = useState(false);
+  const videoRef = useRef<HTMLIFrameElement | null>(null);
+
   const vimeoClasses = cx({
     [`embed-responsive`]: true,
     [`embed-responsive-16by9`]: true,
   });
+
   const iframeClasses = cx({
     [`embed-responsive-item`]: true,
   });
 
-  // The videoUrl prop will be the Vimeo URL of the video
-  //const vimeoId = videoUrl?.match(/\/(\d+)$/)?.[1];
-  // The videoUrl prop will be the Vimeo URL of the video
-const vimeoId = videoUrl?.match(/vimeo\.com\/(\d+)/)?.[1];
+  // Extract Vimeo ID
+  const vimeoId = videoUrl?.match(/vimeo\.com\/(\d+)/)?.[1];
+
+  // Intersection Observer setup
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          const iframe = entry.target as HTMLIFrameElement;
+          if (entry.isIntersecting) {
+            // Play the video when it is in view
+            if (!autoplayed) {
+              iframe.src = iframe.src + '?autoplay=1'; // Append autoplay=1 to the URL
+              setAutoplayed(true);
+            } else {
+              iframe.contentWindow?.postMessage(
+                { method: 'play' },
+                '*'
+              ); // Play the video when it comes back into view
+            }
+          } else {
+            // Pause the video when it goes out of view
+            iframe.contentWindow?.postMessage(
+              { method: 'pause' },
+              '*'
+            ); // Pause the video
+          }
+        });
+      },
+      {
+        root: null,
+        threshold: 0.5, // Trigger when 50% of the video is in view
+      }
+    );
+
+    if (videoRef.current) {
+      observer.observe(videoRef.current);
+    }
+
+    return () => {
+      if (videoRef.current) {
+        observer.unobserve(videoRef.current);
+      }
+    };
+  }, [autoplayed]);
 
   return (
     <div className={styles.the_video}>
       <div className={vimeoClasses}>
         <iframe
+          ref={videoRef}
           className={iframeClasses}
           title="Blinded Flight"
           src={`https://player.vimeo.com/video/${vimeoId}`}
